4a88c6468cfaf23a869f4381754bad3a5eca6479
Lucas Gorski <gorskil@eos14.cis.gvsu.edu>
2018-10-04 22:11:43 -0400

    Messing around with git

--- CardSuite/src/CardSuite/main.java
+++ CardSuite/src/CardSuite/main.java
@@ -6,7 +6,7 @@ public class main {
 	public static void main(String[] args) {
 		//BlackJack game = new BlackJack(250000000,250000, 1, 0, 100);
 		long houseMoney = 10000000000L;
-		BlackJackSimulator game = new BlackJackSimulator(houseMoney, 1000000000L, 3, 1000000000, 100, 4);
+		BlackJackSimulator game = new BlackJackSimulator(houseMoney, 1000000000L, 3, 100, 100, 4);
 		game.game();
 		
 	}
