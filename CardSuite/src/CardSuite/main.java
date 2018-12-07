e4835d3235acf9b6dc7a943e5a6451a1d1631e85
gustafsn <gustafsn@mail.gvsu.edu>
2018-12-07 10:47:29 -0500

    Update main.java

--- CardSuite/src/CardSuite/main.java
+++ CardSuite/src/CardSuite/main.java
@@ -7,7 +7,7 @@ public class main {
 	
 	public static void main(String[] args) {
 		BlackJack game = new BlackJack(250000000,250000, 1, 0, 100);
-		long houseMoney = 10000000000L;
+		//long houseMoney = 10000000000L;
 		//BlackJackSimulator game = new BlackJackSimulator(houseMoney, 1000000000L, 3, 100000000, 100, 4);
 		game.game();
 	}
